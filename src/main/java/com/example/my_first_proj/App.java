fb12cb8e8cf6fa57d113517cfa8a392e050f8c88
ArunGarimella04 <arungarimella04@gmail.com>
2024-12-02 11:13:04 +0530

    Update App.java

--- src/main/java/com/example/my_first_proj/App.java
+++ src/main/java/com/example/my_first_proj/App.java
@@ -4,5 +4,6 @@ package com.example.my_first_proj;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Maven Java project using Poll SCM");
     }
 }
